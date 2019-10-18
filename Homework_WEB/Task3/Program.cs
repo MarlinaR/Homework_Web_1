b9dc6d3eeceb822cfe89978f8e9232ebac3a64b3
ArinaRa <arina030312@mail.ru>
2019-10-18 22:53:19 +1000

    Task3
    task3

---
+++ Homework_WEB/Task3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размер");
+            int size = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите min");
+            int min = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите max");
+            int max = int.Parse(Console.ReadLine());
+            int[] array = new int[size];
+            Random rand = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(min, max);
+                Console.Write(array[i]);
+                Console.Write(' ');
+            }
+                
+
+        }
+    }
+}
