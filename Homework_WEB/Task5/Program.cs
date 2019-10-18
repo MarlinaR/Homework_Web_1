06aa54e3555a1ca7eb1da4ee7aac8c235dd1f519
ArinaRa <arina030312@mail.ru>
2019-10-19 04:32:52 +1000

    Task5/6
    first ver?

---
+++ Homework_WEB/Task5/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace FormulaAggregator
+{
+    class FormulaAggregator
+    {
+        //Запрашивает у пользователя переменную, входной параметр - название переменной (наприме "Х")
+        public static double GetParam(string paramName)
+        {
+            Console.Write("Enter {0} variable: ", paramName);
+            string numInput = Console.ReadLine();
+
+            double goodNumInput = 0;
+            while (!double.TryParse(numInput, out goodNumInput))
+            {
+                Console.Write("Invalid input, enter valid value: ");
+                numInput = Console.ReadLine();
+            }
+
+            return goodNumInput;
+        }
+        public static double DoOperation(int id)
+        {
+            //NaN - Not a Number. используется если функция не сможет вывести результат.
+            double result = double.NaN;
+
+            switch (id)
+            {
+                case 0:
+                    double x = GetParam("x"); //запрашиваем нужные переменные затем выводим ответ
+                    double a = GetParam("a");
+                    double b = GetParam("b");
+                    result = Math.Tan(x) * Math.Tan(2 * x) / (Math.Pow(a, 2) + Math.Pow(b, 2) + 1);
+                    break;
+                default:
+                    break;
+            }
+            return result;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool endApp = false;
+            int MAX_FORMULAS = 1; //Количество формул, расчет на то что считаются с 0
+            double result;
+            Console.WriteLine("Console formula aggregator in C#\r");
+            Console.WriteLine("------------------------\n");
+
+            while (!endApp)
+            {
+                Console.Write("Choose formula: \n");
+                Console.Write("0) y = tg(x) * tg(2x) / (a^2 + b^2 + 1)\n");
+                Console.Write("0) y = tg(x) * tg(2x) / (a^2 + b^2 + 1)\n");
+                //пиши еще формулы, увеличь MAX_FORMULAS
+
+
+                //выбор номера формулы
+                int formulaId = -1;
+                while (formulaId < 0 || formulaId >= MAX_FORMULAS)
+                {
+                    string tempId = Console.ReadLine();
+                    while (!int.TryParse(tempId, out formulaId))
+                    {
+                        Console.Write("Invalid input, enter integer value: ");
+                        tempId = Console.ReadLine();
+                    }
+                    formulaId = Convert.ToInt32(tempId);
+                }
+
+                //вычисления. try catch для выявления ошибок
+                try
+                {
+                    result = FormulaAggregator.DoOperation(formulaId);
+                    if (double.IsNaN(result))
+                    {
+                        Console.WriteLine("Mathematical error.\n");
+                    }
+                    else Console.WriteLine("Your result: {0}\n", result);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Catch an exception.\n - Details: " + ex.Message);
+                }
+
+                Console.WriteLine("------------------------\n");
+
+                Console.Write("Enter 'no' to close the app, or enter any other to continue: ");
+                if (Console.ReadLine() == "no") endApp = true;
+                Console.WriteLine("\n");
+            }
+            return;
+        }
+    }
+}
---
+++ Homework_WEB/Task6/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        class getStringInfo
+        {
+            public static void doTheThings(string str)
+            {
+                
+                int notWhiteSpaceSymbols = 0, wordCount = 0;
+                double symbolToWordRatio;
+                string lastCharsWord = string.Empty;
+
+                for (int i = 1; i < str.Length; i++) //проверка последнего символа потом еще или как подумай
+                {
+                    if (char.IsLetterOrDigit(str[i - 1]) == true)
+                    {
+                        notWhiteSpaceSymbols++;
+                        if (char.IsLetterOrDigit(str[i]) == false)
+                        {
+                            wordCount++;
+                            lastCharsWord += Convert.ToString(str[i - 1]);
+                        }
+                    }
+                    else if (char.IsWhiteSpace(str[i - 1]) == false)
+                        notWhiteSpaceSymbols++;
+                }
+
+                if (char.IsWhiteSpace(str[str.Length-1]) == false)
+                    notWhiteSpaceSymbols++;
+                if (char.IsLetterOrDigit(str[str.Length-1]) == true)
+                {
+                    wordCount++;
+                    lastCharsWord += Convert.ToString(str[str.Length-1]);
+                }
+
+                symbolToWordRatio = Convert.ToDouble(notWhiteSpaceSymbols) / Convert.ToDouble(wordCount);
+
+                Console.WriteLine("Количество слов: {0};", wordCount);
+                Console.WriteLine("Количество символов без пробелов: {0};", notWhiteSpaceSymbols);
+                Console.WriteLine("Соотношение количество символов без пробелов к количеству слов: {0:F2};", symbolToWordRatio);
+                Console.WriteLine("Слово из последних символов слов: \"{0}\";", lastCharsWord);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ведите строку или проганите тест:");
+            string str = Console.ReadLine();
+            if (str == string.Empty)
+            {
+                str = "Lorem Ipsum - это текст-\"рыба\", часто используемый в печати и вэб-дизайне";
+                Console.WriteLine("Тест - "+str+"\n");
+            }
+
+
+            getStringInfo.doTheThings(str);
+
+        }
+    }
+}
